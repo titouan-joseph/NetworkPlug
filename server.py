a965b24a9b8bc90b7f0436648f21b576d3259552
Titouan-joseph Revol <titouanjoseph@gmail.com>
2020-03-15 23:54:39 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-#encoding:utf-8
-#Tutorial: http://www.knight-of-pi.org/cherrypy-an-elegant-python-webserver-for-raspberry-pi-remote-controls/
-#Licence: http://creativecommons.org/licenses/by-nc-sa/3.0/
+# encoding:utf-8
+# Tutorial: http://www.knight-of-pi.org/cherrypy-an-elegant-python-webserver-for-raspberry-pi-remote-controls/
+# Licence: http://creativecommons.org/licenses/by-nc-sa/3.0/
 # Author: Johannes Bergs
 
-import random, string
-from time import sleep
-
 import cherrypy
+import termcolor
+import os
 
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
+
 
 class RaspiServer(object):
     @cherrypy.expose
@@ -19,36 +19,60 @@ class RaspiServer(object):
         return """<html>
           <head></head>
           <body>
-            <form method="get" action="on">
-              <input type="text" value="13" name="pin_str" />
-              <button type="submit">ON</button>
-            </form>
-			<form method="get" action="off">
-              <input type="text" value="13" name="pin_str" />
-              <button type="submit">OFF</button>
+            <form method="get" action="action">
+              <input type="text" name="action" />
+              <button type="submit">OK</button>
             </form>
           </body>
         </html>"""
 
     @cherrypy.expose
-    def off(self, pin_str="13"):
-        pin = int(pin_str)
-        GPIO.setup(pin, GPIO.OUT)        
-        GPIO.output(pin, GPIO.HIGH)
+    def action(self, action):
+        if action == "eteint_3_ecran_bas":
+            self.gpiooff([11, 12, 13])
+            return "Off 11 12 13"
+        elif action == "allume_3_ecran_bas":
+            self.gpioon([11, 12, 13])
+            return  "On 11 12 13"
+        elif action == "eteint_3_ecran_haut":
+            self.gpiooff([7, 8, 5])
+            return "Off 7 8 5 "
+        elif action == "allume_3_ecran_haut":
+            self.gpioon([7, 8, 5])
+            return "On 7 8 5 "
+        elif action == "eteint_ecran":
+            self.gpiooff([7, 8, 5, 11, 12, 13])
+            return "Off 7 8 5 11 12 13"
+        elif action == "allume_ecran":
+            self.gpioon([7, 8, 5, 11, 12, 13])
+            return "On 7 8 5 11 12 13"
+        elif action == "eteint_vive":
+            self.gpiooff([3])
+            return "Off 3"
+        elif action == "allume_vive":
+            self.gpioon([3])
+            return "On 3"
+        elif action == "eteint_pc":
+            self.gpiooff([10])
+            return "Off 10"
 
-        return "Blinked on Pin " + str(pin)
-		
-		
-    @cherrypy.expose
-    def on(self, pin_str="13"):
-        pin = int(pin_str)
-        GPIO.setup(pin, GPIO.OUT)        
-        GPIO.output(pin, GPIO.LOW)
+        else:
+            return "Error, action inconue"
 
-        return "Blinked on Pin " + str(pin) 
+    def gpioon(self, pins):
+        for pin in pins:
+            print(termcolor.colored(str(pin), 'green'))
+            GPIO.setup(pin, GPIO.OUT)
+            GPIO.output(pin, GPIO.LOW)
+
+    @cherrypy.expose()
+    def gpiooff(self, pins):
+        for pin in pins:
+            print(termcolor.colored(str(pin), 'red'))
+            GPIO.setup(pin, GPIO.OUT)
+            GPIO.output(pin, GPIO.HIGH)
 
 
 if __name__ == '__main__':
     cherrypy.config.update({'server.socket_host': '192.168.1.200'})
     cherrypy.quickstart(RaspiServer())
-
